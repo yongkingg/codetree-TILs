d90f7b8102a82f721d8e1c2b714be3f1aa5a2326
YongJunKim <dydwns8064@naver.com>
2024-03-19 00:45:02 +0900

    Title: 모이자, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240319/모이자/gather.py
+++ 240319/모이자/gather.py
@@ -1,15 +1,11 @@
 n = int(input())
-Ai = input()
-aiList = Ai.split()
+Ai = list(map(int,input().split()))
 total = []
-
 for point in range(n):
-    cal = []
-    for x in range(0,n):
+    emptyList = []
+    for x in range(n):
         dx = abs(x - point)
-        peopleDx = dx * int(aiList[x])
-        cal.append(peopleDx)
-    total.append(sum(cal))
-
-
-print(min(total))
+        peopleDx = dx * Ai[x]
+        emptyList.append(peopleDx)
+    total.append(sum(emptyList))
+print(total)
