3967a6a84f0bb87db931219712da67169d84a790
YongJunKim <dydwns8064@naver.com>
2024-04-22 14:23:07 +0900

    Title: 함수를 이용한 온전수 판별, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240422/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,20 @@
+def isMagicNum(n):
+    if n % 2 == 0:
+        return False
+    elif n % 5 == 0:
+        return False
+    elif n % 3 == 0 and n % 9 != 0:
+        return False
+    return True
+
+
+
+
+
+
+a, b = map(int,input().split())
+count = 0
+for index in range(a, b):
+    if isMagicNum(index):
+        count += 1
+print(count)
