d768975c2e94eb765e48c455c907aac6d93bb59a
YongJunKim <dydwns8064@naver.com>
2024-04-25 15:41:36 +0900

    Title: 2021년 날짜의 유무, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240425/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,21 @@
+# 홀수달은 31일까지, 짝수달은 30일까지. 단, 2월은 28일까지 
+def is_correct_day(M,D):
+    if M == 2:
+        if D > 28:
+            return False
+    elif M % 2 == 0:
+        if D > 30:
+            return False
+    else:
+        if D > 31:
+            return False
+    return True
+
+
+
+
+M, D = map(int,input().split())
+if is_correct_day(M, D):
+    print("Yes")
+else:
+    print("No")
