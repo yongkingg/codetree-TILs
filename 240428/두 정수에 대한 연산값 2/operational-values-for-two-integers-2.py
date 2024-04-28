53d13c3503c5bf7c3b6a85013fe940b7f89ed294
YongJunKim <dydwns8064@naver.com>
2024-04-28 19:51:19 +0900

    Title: 두 정수에 대한 연산값 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240428/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,19 @@
+# 변수 선언 및 입력
+a, b = tuple(map(int, input().split()))
+
+
+# 문제에서 설명한 대로 a와 b를 변경합니다.
+def change_number(a, b):
+    if(a > b):
+        b *= 2
+        a += 10
+    else:
+        a *= 2
+        b += 10
+
+    return a, b
+
+
+a, b = change_number(a, b)
+
+print(a, b)
