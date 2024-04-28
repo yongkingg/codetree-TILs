32d806aa74ae457776bc3483df8053fcb97f345c
YongJunKim <dydwns8064@naver.com>
2024-04-28 19:41:33 +0900

    Title: 두 정수에 대한 연산값, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240428/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240428/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,14 +1,19 @@
-def cal(a, b):
-    if a > b:
-        a += 25, 
-        b = b * 2
+# 변수 선언 및 입력
+a, b = tuple(map(int, input().split()))
+
+
+# 문제에서 설명한 대로 a와 b를 변경합니다.
+def change_number(a, b):
+    if(a > b):
+        b *= 2
+        a += 25
     else:
-        a = a * 2
+        a *= 2
         b += 25
+
     return a, b
 
 
+a, b = change_number(a, b)
 
-
-a, b = map(int, input().split())
-print(*cal(a,b))
+print(a, b)
