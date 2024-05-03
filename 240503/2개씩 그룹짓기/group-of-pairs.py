0c7c56d19734f5c13387a1b934153934efb743d3
YongJunKim <dydwns8064@naver.com>
2024-05-03 14:20:04 +0900

    Title: 2개씩 그룹짓기, Time: 112ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/2개씩 그룹짓기/group-of-pairs.py
+++ 240503/2개씩 그룹짓기/group-of-pairs.py
@@ -5,6 +5,6 @@ emptyList = []
 
 # [n-1]번째와 [2n-1]번째의 수를 매칭해주는게 좋음
 
-for index in range(1,N+1):
-    emptyList.append(num[index - 1] + num[2*index - 1])
+for index in range(N):
+    emptyList.append(num[index] + num[2*N - 1 - index])
 print(max(emptyList))
