7de8fe27d93bdea3b3c829340fe30dd2a01395fa
YongJunKim <dydwns8064@naver.com>
2024-05-10 15:26:29 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240510/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 240510/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -9,32 +9,12 @@ class Number:
         self.befPos = befPos
         self.aftPos = aftPos
 
-
-
-
-
 for index in range(len(number)):
     info.append(Number(number[index], index+1, 0))
 
-
 sortedInfo = sorted(info,key= lambda x : x.number)
 
-
 for index in range(len(number)):
-    # info[index].aftPos = index
-    # print(info[index].aftPos, end=" ")
-
     sortedInfo[index].aftPos = index + 1
-
 for index in range(len(number)):
     print(info[index].aftPos, end=" ")
-    
-# print()
-# for index in range(len(number)):
-#     print("SortedInfo : ", end=" ")
-#     print(info[index].number, info[index].befPos, info[index].aftPos)
-
-
-# print()
-# for index in range(len(number)):
-#     print(info[index].aftPos, end=" ")
