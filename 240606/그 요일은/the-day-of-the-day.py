96b414d029df08cc435c9862d24834d6793f4ddb
YongJunKim <dydwns8064@naver.com>
2024-06-06 00:18:59 +0900

    Title: 그 요일은, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240606/그 요일은/the-day-of-the-day.py
+++ 240606/그 요일은/the-day-of-the-day.py
@@ -6,12 +6,15 @@ type = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 days_dif = (sum(days[:m2 - 1]) + d2) - (sum(days[:m1 - 1]) + d1)
 day_idx = type.index(A)
 
-count = 0
-for index in range(abs(days_dif)):
-    tmpType = type[int((index + day_idx + 1) % 7)]
-    print(tmpType)
-    if tmpType == A:
-        print(index + 1)
-        count += 1
 
-print(count)
+
+tmpType = int((days_dif + day_idx + 1) / 7)
+
+
+# count = 0
+# for index in range(abs(days_dif)):
+#     tmpType = type[int((index + day_idx + 1) % 7)]
+#     if tmpType == A:
+#         count += 1
+
+print(tmpType)
