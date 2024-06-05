0609b3dffaee3e3db25694a33bd2c1aa927b7a5b
YongJunKim <dydwns8064@naver.com>
2024-06-06 00:01:57 +0900

    Title: 그 요일은, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240606/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,11 @@
+m1, d1, m2, d2 = map(int,input().split())
+A = input()
+days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+type = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+days_dif = (sum(days[:m2 - 1]) + d2) - (sum(days[:m1 - 1]) + d1)
+day_idx = type.index(A)
+
+value = int((days_dif + day_idx) / 7)
+
+print(value)
