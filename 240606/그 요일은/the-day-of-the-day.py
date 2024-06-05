f77f53534519f94348836722d52574cd795883e8
YongJunKim <dydwns8064@naver.com>
2024-06-06 00:28:57 +0900

    Title: 그 요일은, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240606/그 요일은/the-day-of-the-day.py
+++ 240606/그 요일은/the-day-of-the-day.py
@@ -1,20 +1,21 @@
-m1, d1, m2, d2 = map(int,input().split())
+m1, d1, m2, d2 = map(int, input().split())
 A = input()
 days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 type = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-days_dif = (sum(days[:m2 - 1]) + d2) - (sum(days[:m1 - 1]) + d1)
-day_idx = type.index(A)
+# m1월 d1일의 요일 계산
+start_weekday = type.index(A)
 
+# 목표 요일 계산
+target_weekday = type.index(A)
 
+# 두 날짜의 총 일수 계산
+total_days = (sum(days[:m2 - 1]) + d2) - (sum(days[:m1 - 1]) + d1) + 1
 
-tmpType = int((days_dif + day_idx + 1) / 7)
+# 특정 요일의 등장 횟수 계산
+count = 0
+for day_offset in range(total_days):
+    if (start_weekday + day_offset) % 7 == target_weekday:
+        count += 1
 
-
-# count = 0
-# for index in range(abs(days_dif)):
-#     tmpType = type[int((index + day_idx + 1) % 7)]
-#     if tmpType == A:
-#         count += 1
-
-print(tmpType)
+print(count)
