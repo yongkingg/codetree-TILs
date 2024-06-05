38eecd79827a28326d6a54b8124a286c4ed117f4
YongJunKim <dydwns8064@naver.com>
2024-06-05 23:07:33 +0900

    Title: DateTime to DateTime, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240605/DateTime to DateTime/datetime-to-datetime.py
+++ 240605/DateTime to DateTime/datetime-to-datetime.py
@@ -1,12 +1,15 @@
 a, b, c = map(int,input().split())
 
-days_dif = a - 11
-hours_dif = abs(b - 11)
-min_dif = abs(c - 11)
+if ( a < 11 or b < 11 or c < 11):
+    print("-1")
+else:
+    days_dif = a - 11
+    hours_dif = abs(b - 11)
+    min_dif = abs(c - 11)
 
 
 
-min_dif += days_dif * 24 * 60
-min_dif += hours_dif * 60
+    min_dif += days_dif * 24 * 60
+    min_dif += hours_dif * 60
 
-print(min_dif)
+    print(min_dif)
