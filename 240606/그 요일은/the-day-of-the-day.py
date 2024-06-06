7c4d6c285473636d25d21301b4e28e57dec45647
YongJunKim <dydwns8064@naver.com>
2024-06-06 16:28:43 +0900

    Title: 그 요일은, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240606/그 요일은/the-day-of-the-day.py
+++ 240606/그 요일은/the-day-of-the-day.py
@@ -1,25 +1,12 @@
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
 days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-type = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+weekend = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-# 시작 날짜의 요일을 계산합니다.
-start_day_of_week = type.index(A)
-
-# 결과를 저장할 변수를 초기화합니다.
+days_dif =(sum(days[:m2-1]) + d2) - (sum(days[:m1-1]) + d1)
 count = 0
-
-# 주어진 범위 내의 각 날짜에 대해 요일을 계산하고, A 요일이면 count를 증가시킵니다.
-for month in range(m1, m2 + 1):
-    start_day = d1 if month == m1 else 1
-    end_day = d2 if month == m2 else days[month - 1]
-    
-    for day in range(start_day, end_day + 1):
-        # 현재 날짜의 요일을 계산합니다.
-        current_day_of_week = (start_day_of_week + day - d1) % 7
-        
-        # A 요일과 같으면 count를 증가시킵니다.
-        if current_day_of_week == type.index(A):
-            count += 1
+for index in range(days_dif):
+    if (weekend[(index+1) % 7] == A):
+        count += 1
 
 print(count)
